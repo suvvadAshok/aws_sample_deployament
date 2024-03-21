57825c8991b0b3c39fbf3472e40585b80dc1fa5b
ashokadvait <ashok@advaitlabs.com>
2024-03-21 16:57:18 +0530

    add web app

---
+++ amplify/backend/function/awssampledeployament4fcf61be/src/index.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+  console.log(event);
+  const customerId = event.pathParameters.customerId;
+  const customer = { customerId, custoemrName: "Customer" + customerId };
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify(customer),
+  };
+  return response;
+};
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api49acc64a": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "awssampledeployament4fcf61be": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
